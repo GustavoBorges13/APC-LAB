0a542c15054f451e10a4fe03322d7cd5c21d9351
GustavoBorges13 <63672758+GustavoBorges13@users.noreply.github.com>
2022-02-05 16:25:24 -0300

    Delete Expressoes directory

--- Expressoes/questao_5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-main ( ){
-    char simbolos[55];
-    strcpy (simbolos, "*******************************************************");
-    printf ("%1.23s Bom dia %1.23s \n", simbolos, simbolos);
-    printf ("Hoje foi minha primeira aula de Pratica de Programacao! \n");
-    printf ("%s \n", simbolos);
-}
